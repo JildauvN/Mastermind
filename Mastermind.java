476caf50056d6d80674e933b8ab845aac9e13268
JildauvN <jildau94@hotmail.com>
2020-04-13 11:44:41 +0200

    Eerste

---
+++ Mastermind.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+// Code genereren
+// FD 
+// TS 
+// TS 
+public class Mastermind {
+    public static void main(String[] args) {
+        System.out.println("Welkom bij Mastermind, De eindcode is:");
+        Random ra = new Random();
+		String code = "";
+        for(int i = 0; i < 4; i++){
+            int getal = ra.nextInt(6) + 97;
+           	char a = (char) getal;
+			code = code.concat( ("" + a));
+        }
+		System.out.println(code);
+		
+        
+		Scanner input = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("Voer uw code in");
+        String woord = input.nextLine();
+        char eersteLetterRaden = woord.charAt(0);
+
+
+        System.out.println(eersteLetterRaden);
+          System.out.println("");
+          System.out.println("Bedankt voor het spelen!!");
+    }
+}
